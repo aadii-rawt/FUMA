3d388563415afe98c7bc0229bd62e7766bddbae4
Aditya Rawat <99741576+aadii-rawt@users.noreply.github.com>
2025-11-04 12:11:47 +0530

    remove the dist

--- server/dist/middleware/planValidatin.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-declare const planValidation: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>>>;
-export default planValidation;
-//# sourceMappingURL=planValidatin.d.ts.map
--- server/dist/utils/emailTemplates.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const loginEmailTemplate: (otp: string) => void;
-export declare const signupEmailTemplate: (otp: string) => void;
-//# sourceMappingURL=emailTemplates.d.ts.map
